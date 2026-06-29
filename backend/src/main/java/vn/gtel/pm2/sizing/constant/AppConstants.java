ae2c3159284d2add29c689cb76e73e86c0c4ad89
jade-tam <jade.tam.original@gmail.com>
2026-06-29 17:45:12 +0700

    feat: remove config in code, use exising application.yml config instead

--- backend/src/main/java/vn/gtel/pm2/sizing/constant/AppConstants.java
+++ backend/src/main/java/vn/gtel/pm2/sizing/constant/AppConstants.java
@@ -1,7 +1,6 @@
 package vn.gtel.pm2.sizing.constant;
 
 public final class AppConstants {
-    public static final String API_PREFIX = "/api";
     public static final String JWT_TOKEN_TYPE = "Bearer";
     public static final String PASSWORD_REGEX =
             "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{8,128}$";
