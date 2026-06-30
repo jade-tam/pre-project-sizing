1d26a19f02be716ee43c29dd0ba3e4d3153ba42b
jade-tam <jade.tam.original@gmail.com>
2026-06-30 15:26:03 +0700

    fix: correct

--- backend/src/main/java/vn/gtel/pm2/sizing/entity/BaseEntity.java
+++ backend/src/main/java/vn/gtel/pm2/sizing/entity/BaseEntity.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 
 @MappedSuperclass
 @Getter
-@Setter
 public abstract class BaseEntity {
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
@@ -22,18 +21,23 @@ public abstract class BaseEntity {
     @Column(name = "updated_at")
     private Instant updatedAt;
 
+    @Setter
     @Column(name = "deleted_at")
     private Instant deletedAt;
 
+    @Setter
     @Column(name = "created_by")
     private UUID createdBy;
 
+    @Setter
     @Column(name = "updated_by")
     private UUID updatedBy;
 
+    @Setter
     @Column(name = "deleted_by")
     private UUID deletedBy;
 
+    @Setter
     @Column(name = "deleted")
     private boolean deleted = false;
 }
--- backend/src/main/java/vn/gtel/pm2/sizing/repository/UserRepository.java
+++ backend/src/main/java/vn/gtel/pm2/sizing/repository/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Repository;
 import vn.gtel.pm2.sizing.entity.User;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
