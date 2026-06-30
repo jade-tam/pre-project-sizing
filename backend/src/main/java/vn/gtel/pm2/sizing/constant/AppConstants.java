2787a7e30fd190fdff2ab02884b5fc5a66851f3b
jade-tam <jade.tam.original@gmail.com>
2026-06-30 09:57:06 +0700

    fix: correct password regex

--- backend/src/main/java/vn/gtel/pm2/sizing/constant/AppConstants.java
+++ backend/src/main/java/vn/gtel/pm2/sizing/constant/AppConstants.java
@@ -3,5 +3,5 @@ package vn.gtel.pm2.sizing.constant;
 public final class AppConstants {
     public static final String JWT_TOKEN_TYPE = "Bearer";
     public static final String PASSWORD_REGEX =
-            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{8,128}$";
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{8,128}$";
 }
