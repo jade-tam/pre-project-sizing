bc67a073e020ba9389ffed33179a4ab7d6d684dc
jade-tam <jade.tam.original@gmail.com>
2026-06-30 10:16:55 +0700

    fix: correct openapi config

--- backend/src/main/java/vn/gtel/pm2/sizing/config/OpenApiConfig.java
+++ backend/src/main/java/vn/gtel/pm2/sizing/config/OpenApiConfig.java
@@ -3,17 +3,16 @@ package vn.gtel.pm2.sizing.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import vn.gtel.pm2.sizing.constant.AppConstants;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        String securitySchemeName = "Bearer Authentication";
 
         return new OpenAPI()
                 .info(new Info()
@@ -23,13 +22,10 @@ public class OpenApiConfig {
                 // Define a new security component
                 .components(new Components()
                         .addSecuritySchemes(
-                                securitySchemeName,
+                                AppConstants.SECURITY_SCHEME_NAME,
                                 new SecurityScheme()
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                // Add security component
-                .addSecurityItem(
-                        new SecurityRequirement().addList(securitySchemeName));
+                                        .bearerFormat("JWT")));
     }
 }
--- backend/src/main/java/vn/gtel/pm2/sizing/constant/AppConstants.java
+++ backend/src/main/java/vn/gtel/pm2/sizing/constant/AppConstants.java
@@ -4,4 +4,5 @@ public final class AppConstants {
     public static final String JWT_TOKEN_TYPE = "Bearer";
     public static final String PASSWORD_REGEX =
             "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{8,128}$";
+    public static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
 }
