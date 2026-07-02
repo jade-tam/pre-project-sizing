1863456be3b2acdd3377c390332a0cb1e99ed7ca
jade-tam <jade.tam.original@gmail.com>
2026-07-03 01:07:59 +0700

    feat: add /users/me endpoint

---
+++ backend/src/main/java/vn/gtel/pm2/sizing/controller/UserController.java
@@ -0,0 +1,31 @@
+package vn.gtel.pm2.sizing.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.gtel.pm2.sizing.dto.common.ApiResponse;
+import vn.gtel.pm2.sizing.entity.User;
+import vn.gtel.pm2.sizing.enums.ResponseCode;
+import vn.gtel.pm2.sizing.i18n.MessageService;
+import vn.gtel.pm2.sizing.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final MessageService messageService;
+
+    @GetMapping("/me")
+    public ApiResponse<User> getCurrentUser() {
+        return ApiResponse.success(
+                HttpStatus.OK,
+                ResponseCode.SUCCESS.name(),
+                messageService.get(ResponseCode.SUCCESS.getMessageKey()),
+                userService.getCurrentUser()
+        );
+    }
+}
